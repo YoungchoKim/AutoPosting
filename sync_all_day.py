98138485996e95f30b501a1c2c9e84a0c023eedf
Youngchokim <dudch91@naver.com>
2025-02-02 19:44:24 +0900

    add args in sync_all_day.py

--- sync_all_day.py
+++ sync_all_day.py
@@ -1,10 +1,19 @@
+import argparse
 from sync_one_day import request_post
 from datetime import datetime, timezone, timedelta
 
+
+def init_argparse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--start_date', required=True)
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
+    args = init_argparse()
+
     completed = 0
-    start_date = datetime.strptime('2024-06-23', '%Y-%m-%d').replace(tzinfo=timezone.utc)
-    print(start_date)
+    start_date = datetime.strptime(args.start_date, '%Y-%m-%d').replace(tzinfo=timezone.utc)
     today = datetime.now(timezone.utc)
     while start_date <= today:
         res = request_post(start_date)
